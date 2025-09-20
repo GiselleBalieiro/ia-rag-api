9ade63fca0a4c9d54a7c544032630c0059fce56b
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-09-19 22:17:44 -0300

    fix: ajusta rota

--- index.js
+++ index.js
@@ -236,8 +236,10 @@ app.post("/desconectar", authenticateJWT, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
+
+export default app; 
